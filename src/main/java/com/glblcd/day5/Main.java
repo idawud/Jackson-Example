231374c571c8c759c7e0f0284ed96b2d30d8fde1
idawud <ismaildawud96@gmail.com>
2019-08-02 14:28:23 +0000

    added a client for http request

--- src/main/java/com/glblcd/day5/Main.java
+++ src/main/java/com/glblcd/day5/Main.java
@@ -6,46 +6,71 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Main m = new Main();
-        m.run();
+        String location = "https://xkcd.com/info.0.json";
+        var client = HttpClient.newBuilder().build();
+        URI uri = null;
+        try {
+            uri = new URI(location);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        var req = HttpRequest.newBuilder(uri).build();
+
+        try {
+            var response = client.send(req, HttpResponse.BodyHandlers.ofString(Charset.defaultCharset()));
+            var jsonData = parseJsonToMap(response.body());
+            for (String key : jsonData.keySet()) {
+                System.out.println( key + ": " + jsonData.get(key));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     private void run() {
-        String json = "";
+        parseJsonToMap(loadJsonStringFromFile("src/main/resources/cake.json"));
+    }
+
+    private String loadJsonStringFromFile(String sourcePath){
+        String json = null;
         try {
-            byte[] contents = Files.readAllBytes(Paths.get("src/main/resources/cake.json"));
+            byte[] contents = Files.readAllBytes(Paths.get(sourcePath));
             json = new String(contents);
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
         }
-        // System.out.println(json);
-
-        convertStringToJson(json);
+        return json;
     }
 
-    private void convertStringToJson(String jsonString) {
-        // ObjectMapper mapper = new ObjectMapper();
-        // TypeReference<HashMap<String, Object>> typeRef = new TypeReference<HashMap<String, Object>>() {};
-
+    public static Map<String, Object> parseJsonToMap(String json) {
         var mapper = new ObjectMapper();
-        var typeRef = new TypeReference<HashMap<String, Object>>() {};
+        var typeRef = new TypeReference<HashMap<String,Object>>() {};
+
         try {
-            var mJson = mapper.readValue(jsonString, typeRef);
-            System.out.println(mJson);
-        } catch (JsonParseException e) {
-            e.printStackTrace();
-        } catch (JsonMappingException e) {
-            e.printStackTrace();
+            Map<String, Object> mJson = mapper.readValue(json, typeRef);
+            // System.out.println(mJson);
+            return mJson;
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return null;
     }
 
 }
